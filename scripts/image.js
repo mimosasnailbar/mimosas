d4e13d5842fa919b192799523a89ce422c35eebc
mimosasnailbar <49132518+mimosasnailbar@users.noreply.github.com>
2019-11-15 09:00:09 -0800

    Update image.js

--- scripts/image.js
+++ scripts/image.js
@@ -1,1 +1,13 @@
 const mainImage = document.querySelector('.home-images').src;
+
+const image = new Vue({
+	el: '#imageToggle',
+	data: {
+		src: mainImage
+	},
+	methods: {
+		toggle: function() {
+			this.src = event.target.src;
+		}
+	}
+});
